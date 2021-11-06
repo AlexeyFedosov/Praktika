aa2e2b4823e5e3fa459f4d9ad2d5b765aae312c5
AlexeyFedosov <fedosov_19@mail.ru>
2021-11-06 12:02:40 +0300

    Ex1

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// Cформировать случайным образом целочисленный массив A из натуральных двузначных чисел. 
+//Создать на его основе масив B, отбрасывая те, которые
+// 1.Нарушают порядок возрастания
+// 2.Больше среднего арифметического элементов A
+// 3.Чётные
+int length = 50;
+int[] arrayA = new int[length];
+int indexA = 0;
+while (indexA < length)                      //заполняем первый массив
+{
+    arrayA[indexA] = new Random().Next(10, 100);
+    indexA++;
+}
+
+indexA = 0;
+while (indexA < length)                      //выводим первый массив
+{
+    Console.WriteLine($"A{indexA + 1} = {arrayA[indexA]}");
+    indexA++;
+}
